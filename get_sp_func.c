1850e0c8b99b539db2079c09832dd794193dcd17
sham163 <sh.prin1698@gmail.com>
2023-09-17 15:20:53 -0700

    s

--- get_sp_func.c
+++ get_sp_func.c
@@ -1,5 +1,35 @@
 #include "main.h"
 
+
+/**
+* Description: _strcmp - a function that compares two strings.
+*
+* @s1: char input parameter.
+* @s2: char input parameter.
+*
+* Return: 0 if s1 and s2 are the same;
+* less than 0 if s1<s2; greater than 0 if s1>s2.
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+int equal = 0;
+
+while (*s1)
+{
+if (*s1 != *s2)
+{
+equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+break;
+}
+s1++;
+s2++;
+}
+return (equal);
+}
+
+
+
 /**
 * Description: get_sp - get the specifire
 * @s: string
@@ -18,7 +48,7 @@ int i = 0;
 
 while (sps[i].sp[0])
 {
-(strcmp(s, sps[i].sp) == 0)
+if(_strcmp(s, sps[i].sp) == 0)
 return (sps[i].f);
 i++;
 }
