ee4ca262f2c2b5ab9922bc881aac39fa6f8ebe48
sham163 <sh.prin1698@gmail.com>
2023-09-17 15:06:46 -0700

    s

--- _printf.c
+++ _printf.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
 int _putchar(char c)
 {
 return (write(1, &c, 1));
@@ -25,34 +25,35 @@ char *ptr;
 int sum = 0;
 
 va_start(ss, format);
+
 while (*format != '\0')
 {
 if (*format == '%')
 {
 format++;
 ptr = (char *)format;
-if (*ptr != '\0')
-{
+
 int (*func)(va_list) = get_sp(ptr);
 if (func)
 {
 sum += func(ss);
-format += 1;
+format += 1; 
 }
 else
 {
-_putchar('%');
-sum += 1;
-}
+
+write(1, "%%", 2); 
+sum += 2;
 }
 }
 else
 {
-_putchar(*format);
-sum += 1;
+write(1, format, 1);
+sum++;
 }
 format++;
 }
+
 va_end(ss);
-return (sum);
+return sum;
 }
